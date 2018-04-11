bf818671bc4a63b3e6ae3a7406a12316c46235c4
David Raygoza Gómez <raygoza.gomez@gmail.com>
2018-04-11 16:19:56 -0500

    Version 1.1
    Se verifica que se halla seleccionado un renglon antes de imprimir

--- src/main/java/mx/hashCode/table/Tabla.java
+++ src/main/java/mx/hashCode/table/Tabla.java
@@ -29,19 +29,19 @@ public class Tabla extends JFrame implements ActionListener {
 
         // Ponemos el nombre de las columnas en el modelo
         modelo.setColumnIdentifiers(columnas);
-        
+
         // Establecemos el modelo de la tabla
         tabla.setModel(modelo);
 
         // Agregamos datos a la tabla
         this.agregarDatos();
-        
+
         // Configuramos la ventana principal
         this.prepararVentana();
-        
+
         // Agregamos elementos a la interfaz
         this.agregarElementos();
-        
+
         // Agregamos el manejador de evento al boton
         boton.addActionListener(this);
     }
@@ -80,14 +80,19 @@ public class Tabla extends JFrame implements ActionListener {
     /**
      * Manejador del evento del boton, al activarse obtiene un dato de la tabla
      * y lo imprime en la consola
+     *
      * @param e
      */
     public void actionPerformed(ActionEvent e) {
         // Obtenemos el primer dato del renglon seleccionado
-        String codigo = (String) modelo.getValueAt(tabla.getSelectedRow(), 0);
-        
-        // Lo imprimimos en pantalla
-        System.out.println(codigo);
+        if (tabla.getSelectedRow() != -1) {
+            String codigo = (String) modelo.getValueAt(tabla.getSelectedRow(), 0);
+
+            // Lo imprimimos en pantalla
+            System.out.println(codigo);
+        } else {
+            System.out.println("Seleccione un renglon primero");
+        }
     }
 
 }
